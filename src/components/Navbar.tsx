50c003e356bf1cec63ff1d840f597b7ce4194c26
AnaniyaLegesse <ananiya.legesse1@gmail.com>
2024-10-17 13:09:29 +0300

    Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between align-center px-5 h-[15vh]'>
-          <img src='/logo2.svg' className='w-20 h-20' alt='logo' />
+    <div className='flex justify-between align-center px-5 h-[10vh] sticky top-0 bg-white'>
+          <img src='/logo.svg' className='w-20 h-15' alt='logo' />
           <div className="flex gap-4 justify-center items-center">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#services">Services</a> 
-            <a href="/">Contact</a>
+            <a href="/" className=" hover:text-green-800">Home</a>
+            <a href="#about" className=" hover:text-green-800">About</a>
+            <a href="#services" className=" hover:text-green-800">Services</a> 
+            <a href="#contact" className=" hover:text-green-800">Contact</a>
           </div>
     </div>
   )
